1b20ddf19cce91163b4aa36171f7452e5a3fb95c
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-26 04:03:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,6 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize! << ":"
     holiday.each do |holiday, supplies|
@@ -75,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
     end
   end
 end
-end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
